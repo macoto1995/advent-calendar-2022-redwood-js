11bd2704e33d72dea4f0b5dbfe97efdfe37179a6
macoto1995 <adosute07@gmail.com>
2022-11-27 20:18:48 +0900

    post migration

---
+++ api/db/migrations/20221127111820_post/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "UserExample" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserExample_email_key" ON "UserExample"("email");
